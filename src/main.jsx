08f590e8e446dee63d5006f89b0219b5a9cd5e45
hoangdanh165 <hoangdanh.165@gmail.com>
2025-03-17 17:03:32 +0700

    fix production error (theme provider)

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { CssBaseline, ThemeProvider } from '@mui/material';
+import { CssBaseline, Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material';
 
 import router from './routes/router';
 import './index.css';
@@ -13,10 +13,12 @@ const CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <GoogleOAuthProvider clientId={CLIENT_ID}>
-      <CssBaseline />
-        <AuthProvider>
-          <RouterProvider router={router} />
-        </AuthProvider>
+    <CssVarsProvider>
+        <CssBaseline />
+          <AuthProvider>
+            <RouterProvider router={router} />
+          </AuthProvider>
+        </CssVarsProvider>
     </GoogleOAuthProvider>
   </React.StrictMode>
 );
